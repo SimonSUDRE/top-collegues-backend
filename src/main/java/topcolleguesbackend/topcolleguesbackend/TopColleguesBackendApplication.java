0e509436dd884c1137cceeebb25091211910ca58
SimonSUDRE <sta.sudre@gmail.com>
2018-02-15 10:28:29 +0100

    first commit

---
+++ src/main/java/topcolleguesbackend/topcolleguesbackend/TopColleguesBackendApplication.java
@@ -0,0 +1,12 @@
+package topcolleguesbackend.topcolleguesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopColleguesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopColleguesBackendApplication.class, args);
+	}
+}
